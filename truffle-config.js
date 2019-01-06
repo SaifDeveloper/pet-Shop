fe15d616fb341e569c42c46974ec37a22a2cbbe5
ssaifali <ssaifali@in.ibm.com>
2019-01-06 12:44:41 +0530

    ready for heroku

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const HDWalletProvider = require("truffle-hdwallet-provider");
+require('dotenv').config();
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -8,6 +10,10 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*" // Match any network id
+    },
+    rinkeby:{
+      provider: () => new HDWalletProvider(process.env.MNEMONIC,"https://rinkeby.infura.io/v3/"+ process.env.INFURA_API_KEY),
+      network_id:4 
     }
   },
   compilers: {
